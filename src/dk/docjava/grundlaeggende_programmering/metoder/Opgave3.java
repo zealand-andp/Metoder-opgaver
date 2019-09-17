a124fd8b2bd7be7424bbe80ba9044cecfa1d6916
Anders Petersen <AndP@easj.dk>
2019-09-17 10:29:14 +0200

    Test cases opstillet for forskellige input

--- src/dk/docjava/grundlaeggende_programmering/metoder/Opgave3.java
+++ src/dk/docjava/grundlaeggende_programmering/metoder/Opgave3.java
@@ -1,8 +1,22 @@
 package dk.docjava.grundlaeggende_programmering.metoder;
 
 public class Opgave3 {
+    /* Lav en metode midterst, der tager tre tal som parametre.
+    Metoden skal returnere den midterste værdi.
+    F.eks. skal kaldet midterst( 6, 10, 8 ) returnere 8. */
 
     public static void main(String[] args) {
-	// write your code here
+	    // Test case hvor sidste værdi er midterst
+        // Input: a = 6, b = 10, c = 8
+        // Forventet output: 8
+
+        // Test case hvor første værdi er midterst
+        // Input: a = 3, b = 7, c = 2
+        // Forventet output: 3
+
+        // Test case hvor midterste værdi er midterst
+        // Input: a = 10, b = 25, c = 50
+        // Forventet output: 25
+
     }
 }
